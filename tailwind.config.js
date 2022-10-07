d68262363cea0797ebee206910817e45d4f69c4d
Petya <petianaidenova@gmail.com>
2022-10-07 11:53:01 +0200

    remove underline

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      sans: ['Montserrat', 'Roboto'],
+      alternative: ['Montserrat Alternates', 'Roboto'],
+    },
     screens: {
       xs: {max: '425px'},
       sm: {max: '600px'},
@@ -98,10 +102,6 @@ module.exports = {
         xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 8px 10px -6px rgba(0, 0, 0, 0.1)',
         '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
         inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.05)',
-      },
-      fontFamily: {
-        sans: ['Montserrat', 'Roboto'],
-        alternative: ['Montserrat Alternates', 'Roboto'],
       },
       colors: {
         dp: {
